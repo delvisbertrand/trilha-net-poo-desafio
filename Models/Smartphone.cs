33963fa21439fa2ead77679fd92e21b0ed760b77
Delvis Bertrand <delvis.bertrand.dbjb@gmail.com>
2024-05-03 16:39:39 -0300

    fix: Desafio concluído

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,12 +3,17 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
 
-        public Smartphone(string numero)
+        private string modelo;
+        private string imei;
+        private int memoria;
+
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            this.modelo = modelo;
+            this.imei = imei;
+            this.memoria = memoria;
         }
 
         public void Ligar()
